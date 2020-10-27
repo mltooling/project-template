ebbacbbc743e73f4f78d8119fb2552b8c37be934
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-10-27 17:52:37 +0100

    Restructure project based on the description in the README file

--- react-webapp/src/index.jsx
+++ react-webapp/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/App/App';
+import reportWebVitals from './utils/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
