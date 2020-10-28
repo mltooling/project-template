f595ab73e8ccdd4306edc651a3a9f362847384f5
Lukas Masuch <lukas.masuch@gmail.com>
2020-10-29 00:10:41 +0100

    Improv get latest version action

--- .github/actions/get-latest-version/entrypoint.sh
+++ .github/actions/get-latest-version/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 ADDITIONAL_OPTIONS=""
 
-if [[ $INPUT_CURRENT_COMMIT_ONLY = true ]] ; then
+if [[ $INPUT_CURRENT_COMMIT_ONLY = true ]]; then
     ADDITIONAL_OPTIONS="--exact-match"
 fi
 
@@ -13,12 +13,12 @@ LATEST_VERSION=$(git describe --tags --match 'v[0-9].*' --abbrev=0 $ADDITIONAL_O
 # LATEST_VERSION="git tag --points-at HEAD --sort -version:refname | head -1"
 # --exact-match
 
-if [[ ! "$LATEST_VERSION" =~ ^v([0-9]+\.[0-9]+\.[0-9]+.*)$ ]] ; then
+if [[ ! "$LATEST_VERSION" =~ ^v([0-9]+\.[0-9]+\.[0-9]+.*)$ ]]; then
     echo "The latest tag is not valid: $LATEST_VERSION"
     exit 1
 fi
 
 LATEST_VERSION=$(echo $LATEST_VERSION | sed -E 's/v([0-9]+\.[0-9]+\.[0-9]+.*)/\1/')
 
-echo "::set-output name=version::$LATEST_VERSION"
-echo "::set-env name=LATEST_VERSION::$LATEST_VERSION"
+echo "::set-output name=latest_version::$LATEST_VERSION"
+# echo "::set-env name=LATEST_VERSION::$LATEST_VERSION"
