c83629e80891db78372d7daaa279076042646339
Lukas Masuch <lukas.masuch@gmail.com>
2020-10-29 18:07:08 +0100

    Cleanup get latest version entrypoint

--- .github/actions/get-latest-version/entrypoint.sh
+++ .github/actions/get-latest-version/entrypoint.sh
@@ -10,8 +10,6 @@ if [[ $INPUT_CURRENT_COMMIT_ONLY = true ]]; then
 fi
 
 LATEST_VERSION=$(git describe --tags --match 'v[0-9].*' --abbrev=0 $ADDITIONAL_OPTIONS)
-# LATEST_VERSION="git tag --points-at HEAD --sort -version:refname | head -1"
-# --exact-match
 
 if [[ ! "$LATEST_VERSION" =~ ^v([0-9]+\.[0-9]+\.[0-9]+.*)$ ]]; then
     echo "The latest tag is not valid: $LATEST_VERSION"
@@ -21,4 +19,3 @@ fi
 LATEST_VERSION=$(echo $LATEST_VERSION | sed -E 's/v([0-9]+\.[0-9]+\.[0-9]+.*)/\1/')
 
 echo "::set-output name=latest_version::$LATEST_VERSION"
-# echo "::set-env name=LATEST_VERSION::$LATEST_VERSION"
