f99c5a652094cb319d4eaec2e16449ea7a2283bf
Lukas Masuch <lukas.masuch@gmail.com>
2020-10-29 19:49:24 +0100

    Add build.py to root folder

--- build.py
+++ build.py
@@ -1,5 +1,23 @@
+from typing import Dict, Union
+
 from universal_build import build_utils
 
-args = build_utils.get_sanitized_arguments()
 
-build_utils.build("react-webapp", args)
+def main(args: Dict[str, Union[bool, str]]):
+    build_utils.build("docs", args)
+    build_utils.build("react-webapp", args)
+    build_utils.build("java-service", args)
+    build_utils.build("python-lib", args)
+
+
+if __name__ == "__main__":
+    # Check for valid arguments
+    args = build_utils.get_sanitized_arguments()
+
+    if args[build_utils.FLAG_RELEASE]:
+        # Run main without release to see whether everthing can be built and all tests run through
+        main({**args, "release": False})
+        # Run main again with only executing release with force flag
+        main({**args, "make": False, "test": False, "check": False, "force": True})
+    else:
+        main(args)
