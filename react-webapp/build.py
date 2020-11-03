e4e5ae9640a7cb189bb7440e2512f4172ea17805
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-03 10:37:02 +0100

    Use exit on error in react build script

--- react-webapp/build.py
+++ react-webapp/build.py
@@ -7,25 +7,14 @@ build_utils.run("yarn install")
 
 if args[build_utils.FLAG_CHECK]:
     build_utils.log("Run linters:")
-    js_linting_completed_process = build_utils.run("yarn run lint:js")
-    style_linting_completed_process = build_utils.run("yarn run lint:css")
-
-    if (
-        js_linting_completed_process.returncode > 0
-        or style_linting_completed_process.returncode > 0
-    ):
-        build_utils.log("One of the linters threw an error")
-        build_utils.exit_process(1)
-
+    build_utils.run("yarn run lint:js", exit_on_error=True)
+    build_utils.run("yarn run lint:css", exit_on_error=True)
     build_utils.log("No linter problems")
 
 if args[build_utils.FLAG_MAKE]:
-    build_utils.log("Build the webapp")
-    completed_process = build_utils.run("yarn build")
-    if completed_process.returncode > 0:
-        build_utils.log("Building the webapp failed")
-        build_utils.exit_process(completed_process.returncode)
+    build_utils.log("Build the webapp:")
+    build_utils.run("yarn build", exit_on_error=True)
 
 if args[build_utils.FLAG_TEST]:
-    build_utils.log("Test the webapp")
-    completed_process = build_utils.run("yarn test a --watchAll=false")
+    build_utils.log("Test the webapp:")
+    build_utils.run("yarn test a --watchAll=false", exit_on_error=True)
