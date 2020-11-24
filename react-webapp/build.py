451a9b5198fbc0e07e47c42889aaf2e77354bb60
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-24 14:25:17 +0100

    Use get to get build flags

--- react-webapp/build.py
+++ react-webapp/build.py
@@ -5,16 +5,16 @@ args = build_utils.get_sanitized_arguments()
 build_utils.log("Install essentials")
 build_utils.run("yarn install")
 
-if args[build_utils.FLAG_CHECK]:
+if args.get(build_utils.FLAG_CHECK):
     build_utils.log("Run linters:")
     build_utils.run("yarn run lint:js", exit_on_error=True)
     build_utils.run("yarn run lint:css", exit_on_error=True)
     build_utils.log("No linter problems")
 
-if args[build_utils.FLAG_MAKE]:
+if args.get(build_utils.FLAG_MAKE):
     build_utils.log("Build the webapp:")
     build_utils.run("yarn build", exit_on_error=True)
 
-if args[build_utils.FLAG_TEST]:
+if args.get(build_utils.FLAG_TEST):
     build_utils.log("Test the webapp:")
     build_utils.run("yarn test", exit_on_error=True)
